11ebf369e18ddf5a63e9c8ffe9c63802b0b2d1fc
Dominik Kadera <dominik.kadera@integromat.com>
2019-01-04 11:56:10 +0100

    Token denied

--- background.mjs
+++ background.mjs
@@ -1,14 +1,20 @@
 async function login() {
-	/* const token = await (await fetch('https://www.integromat.com/api/user/api/token', {
-						method: 'POST',
-						body: {
-							scope: [
-								"apps:read",
-								"apps:write"
-							]
-						}
-					})).text();
-					alert(token); */
+	const token = await (await fetch('https://www.integromat.com/api/user/api/token', {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json; charset=UTF-8',
+			'Referer': 'https://www.integromat.com'
+		},
+		credentials: 'include',
+		body: JSON.stringify({
+			label: 'IMT Importer',
+			scope: [
+				'apps:read',
+				'apps:write'
+			]
+		})
+	})).text();
+	alert(`Obtained new token: ${token}`);
 	chrome.storage.local.set({ 'imt_apiKey': '1d6878ad-3a22-4b5d-bec4-7a4c258d3241' }, function () { });
 	chrome.runtime.sendMessage({
 		success: true
