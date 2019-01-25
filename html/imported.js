be5922f28b527b4073f17ec7d6c899deb7d1eb5d
Medard Hüttenbach <medard@cyberpanda.cz>
2019-01-25 13:15:42 +0100

    imported tab style

--- html/imported.js
+++ html/imported.js
@@ -2,7 +2,7 @@ chrome.runtime.onMessage.addListener((message) => {
 	if (message.routine === 'setErrors') {
 		const errorWrapper = document.getElementById('errors');
 		for (const error of message.errors) {
-			errorWrapper.innerHTML += `<a class="doc" target="_blank" href="https://docs.integromat.com/apps/primary/zapier-importer/errors/${error.code}">${error.description} - Severity: ${error.severity}</a><br>`
+			errorWrapper.innerHTML += `<a class="doc" target="_blank" href="https://docs.integromat.com/apps/primary/zapier-importer/errors/${error.code}">${error.description} - Severity: ${error.severity}</a>`
 		}
 	}
 })
