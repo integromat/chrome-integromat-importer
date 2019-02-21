5485baa6ba9d878f9e9fe73be0a168e263b7d939
Dominik Kadera <dominik.kadera@integromat.com>
2019-02-21 14:38:46 +0100

    error/warning class

--- html/imported.js
+++ html/imported.js
@@ -2,8 +2,9 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
 	if (message.routine === 'setErrors') {
 		const errorWrapper = document.getElementById('errors');
 		for (const error of message.errors) {
+			const kind = error.severity < 3 ? 'warning' : 'error';
 			errorWrapper.innerHTML += `<li>
-			<a class="doc" target="_blank" href="https://docs.integromat.com/apps/integromat-importer/errors/${error.code}">${error.description} - Severity: ${error.severity}</a>
+			<a class="doc ${kind}" target="_blank" href="https://docs.integromat.com/apps/integromat-importer/errors/${error.code}">${error.description} - Severity: ${error.severity}</a>
 			<a class="f-right go-to-docs btn xs" target="_blank" href="https://docs.integromat.com/apps/integromat-importer/errors/${error.code}">Open in Docs</a>
 			</li>`
 		}
