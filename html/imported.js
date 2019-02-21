ffa6ab393802536e2117db77773595b90ad7125f
Medard Hüttenbach <medard@cyberpanda.cz>
2019-02-21 15:10:53 +0100

    warning vs error style

--- html/imported.js
+++ html/imported.js
@@ -3,8 +3,8 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
 		const errorWrapper = document.getElementById('errors');
 		for (const error of message.errors) {
 			const kind = error.severity < 3 ? 'warning' : 'error';
-			errorWrapper.innerHTML += `<li>
-			<a class="doc ${kind}" target="_blank" href="https://docs.integromat.com/apps/integromat-importer/errors/${error.code}">${error.description} - Severity: ${error.severity}</a>
+			errorWrapper.innerHTML += `<li class="${kind}">
+			<a class="doc" target="_blank" href="https://docs.integromat.com/apps/integromat-importer/errors/${error.code}">${error.description} - Severity: ${error.severity}</a>
 			<a class="f-right go-to-docs btn xs" target="_blank" href="https://docs.integromat.com/apps/integromat-importer/errors/${error.code}">Open in Docs</a>
 			</li>`
 		}
